48b10f1636436da6add61a6e9c6970c4d04922f8
Sushin Pv <sushinpv@gmail.com>
2022-06-17 16:23:46 +0530

    updated test cases

--- src/components/counter/__test__/counter-view.test.tsx
+++ src/components/counter/__test__/counter-view.test.tsx
@@ -1,8 +1,10 @@
 import { render } from "@testing-library/react";
+import Multiplier from "../../multiplier";
+import Number from "../../number";
 import CounterView from "../counter-view";
 
 test("on Click of function the counter value should increase", () => {
-  const { getByText, getByTestId } = render(<CounterView counter={0} onClick={() => {}} />);
+  const { getByText, getByTestId } = render(<CounterView counter={0} onClick={() => {}} components={{ Multiplier, Number }} />);
 
   const counter = getByText(/counter/i);
   const click = getByTestId("counter-click");
